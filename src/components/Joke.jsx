eed331afaefad14a9d2c6c2adc712267741aa62d
CandyTale55 <candytale55@gmail.com>
2025-07-07 20:40:42 +0200

    feat: add conditional rendering for setup

--- src/components/Joke.jsx
+++ src/components/Joke.jsx
@@ -3,9 +3,9 @@ export default function Joke({setup, punchline}) {
     return (
 
         <article>
-            <h2>I'm Joke</h2>
-            <p>{setup ? setup: ""}</p>
-            <p>{punchline}</p>
+            {setup && <p className="setup">{setup}</p>}
+            <hr />
+            <p className="punchline">{punchline}</p>
         </article>
     )
 }
